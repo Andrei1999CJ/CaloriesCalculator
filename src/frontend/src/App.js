359f033ce483a6bbcea69e251b78765ad8ba694b
Andrei1999CJ <cojocariuandrei32@gmail.com>
2023-06-15 11:21:40 +0300

    Added react components

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { Button, Space } from 'antd';
+import LayoutComp from './components/Layout';
+import AccountPage from './components/AccountPage';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [state, setState] = useState(false);
+    const [showLayout, setShowLayout] = useState(false);
+
+    const SendButton =  () => {
+        if (!state) {
+        return <AccountPage state = {state} setState = {setState}/>;
+        }
+
+    }
+
+
+    useEffect(() => {
+         if (state) {
+            console.log("buton apasat");
+            setShowLayout(true);
+         }
+    }, [state])
+
+   return <>
+   <SendButton/>
+   <LayoutComp showLayout = {showLayout} />
+   </>
+
 }
 
 export default App;
